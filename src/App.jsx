b746123c6b7c027700db0a0a884ae7995f370786
Nikhil Mahajan <mahajan12647@gmail.com>
2023-12-01 00:19:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className="text-9xl font-bold underline h-screen text-center">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
